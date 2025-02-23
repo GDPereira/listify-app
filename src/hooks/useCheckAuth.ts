7308e2d39d7d3dbc74487a965fa7f7cc8a1b35d1
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 11:24:41 -0300

    feat: created checkauth hook

---
+++ src/hooks/useCheckAuth.ts
@@ -0,0 +1,25 @@
+import { getCheck } from "@/src/services/checkAuthData";
+import { useQuery } from "@tanstack/react-query";
+import { HttpStatusCode, isAxiosError } from "axios";
+
+const checkAuthKey = "checkAuth";
+
+export const useCheckAuth = () => {
+  const { data, error, isLoading } = useQuery({
+    queryKey: [checkAuthKey],
+    queryFn: getCheck,
+    retry: (count, error) => {
+      if (count >= 3) {
+        return false;
+      }
+
+      if (isAxiosError(error)) {
+        return error.status !== HttpStatusCode.Unauthorized;
+      }
+
+      return true;
+    },
+  });
+
+  return { data, error, isLoading };
+};
