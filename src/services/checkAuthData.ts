a56137a60fe80eecfaa2e12d36694d64f2b3401a
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 11:22:09 -0300

    feat: created check request

---
+++ src/services/checkAuthData.ts
@@ -0,0 +1,17 @@
+import { api } from "../lib/http";
+
+interface CheckResponse {
+  success: boolean;
+  data: {
+    user: {
+      name: string;
+      userId: string;
+    };
+  };
+}
+
+export const getCheck = async () => {
+  const { data } = await api.get<CheckResponse>("/auth/check");
+
+  return data;
+};
