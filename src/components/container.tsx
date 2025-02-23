c4a800496e8e690cea99668fd4ba89ffea7bf6f1
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 01:29:06 -0300

    feat: added new components

---
+++ src/components/container.tsx
@@ -0,0 +1,26 @@
+import { PropsWithChildren } from "react";
+import { StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  SafeAreaViewProps,
+} from "react-native-safe-area-context";
+
+interface ContainerProps extends SafeAreaViewProps {}
+
+export const Container = ({
+  children,
+  ...props
+}: PropsWithChildren<ContainerProps>) => {
+  return (
+    <SafeAreaView style={styles.container} {...props}>
+      {children}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+});
---
+++ src/components/form/input.tsx
@@ -0,0 +1,50 @@
+import { Controller, useFormContext } from "react-hook-form";
+import { StyleSheet, View } from "react-native";
+import { Text, TextInput, TextInputProps, useTheme } from "react-native-paper";
+
+interface InputProps extends TextInputProps {
+  name: string;
+}
+
+export const Input = ({ name, ...props }: InputProps) => {
+  const { colors } = useTheme();
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+
+  return (
+    <>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => {
+          return (
+            <TextInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              {...props}
+            />
+          );
+        }}
+        name={name}
+      />
+      <View style={styles.errorContainer}>
+        {errors[name] && (
+          <Text variant="labelSmall" style={{ color: colors.error }}>
+            {errors[name].message as string}
+          </Text>
+        )}
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  errorContainer: {
+    height: 40,
+  },
+});
