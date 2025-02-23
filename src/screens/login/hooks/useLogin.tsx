1e958790833d85147697cd758bd6c25f41944fe3
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 11:24:27 -0300

    feat: updated use login hook

--- src/screens/login/hooks/useLogin.tsx
+++ src/screens/login/hooks/useLogin.tsx
@@ -1,14 +1,22 @@
 import { LoginValues, postLogin } from "@/src/services/loginData";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const useLogin = () => {
   const router = useRouter();
 
   const { mutate, isPending, data } = useMutation({
     mutationFn: postLogin,
-    onSuccess: () => {
-      router.replace("/(authenticated)/(tabs)/home");
+    onSuccess: ({ cookies }) => {
+      cookies?.forEach((cookie) => {
+        const [key, value] = cookie.split("=");
+        const [token] = value.split(";");
+
+        AsyncStorage.setItem(key, token);
+      });
+
+      router.replace("/(authenticated)/(tabs)");
     },
   });
 
