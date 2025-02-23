d3fe9f395642ea9ac8c0ace271f1243f57379428
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 11:25:06 -0300

    feat: created user tab screen

---
+++ src/screens/user/userScreen.tsx
@@ -0,0 +1,32 @@
+import { Container } from "@/src/components/container";
+import { useLogout } from "@/src/hooks/useLogout";
+import { Feather } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
+import { Card, Text } from "react-native-paper";
+
+export const UserScreen = () => {
+  const { logout } = useLogout();
+
+  const onClickLogout = () => {
+    logout();
+  };
+
+  return (
+    <Container>
+      <Card onPress={onClickLogout}>
+        <Card.Content style={styles.cardRow}>
+          <Text variant="titleMedium">Logout</Text>
+          <Feather name="log-out" size={24} />
+        </Card.Content>
+      </Card>
+    </Container>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
