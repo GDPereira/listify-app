449ac2e5e517e6d87991e6709e80dfdf9b0fd0d6
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 11:24:12 -0300

    feat: added cookies to requests

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,17 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.EXPO_PUBLIC_API_URL,
   withCredentials: true,
+});
+
+api.interceptors.request.use(async (request) => {
+  const token = await AsyncStorage.getItem("paseto");
+
+  request.headers["cookie"] = `paseto=${token}`;
+
+  return request;
 });
 
 export { api };
