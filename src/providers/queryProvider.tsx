835fb30d71e72d90baa5a4ba82274e8b2175ec6c
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 23:37:41 -0300

    feat: added error handler for query

--- src/providers/queryProvider.tsx
+++ src/providers/queryProvider.tsx
@@ -1,18 +1,36 @@
 import {
   MutationCache,
+  QueryCache,
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { PropsWithChildren, useMemo } from "react";
 import { useDialogStore } from "../store/dialog";
-import { isAxiosError } from "axios";
+import { HttpStatusCode, isAxiosError } from "axios";
+import { useRouter } from "expo-router";
+import { useCookies } from "../hooks/useCookies";
 
 export const QueryProvider = ({ children }: PropsWithChildren) => {
   const { setNotification } = useDialogStore();
+  const router = useRouter();
+  const { clearCookies } = useCookies();
 
   const queryClient = useMemo(
     () =>
       new QueryClient({
+        queryCache: new QueryCache({
+          onError: (error) => {
+            if (isAxiosError(error)) {
+              const isUnauthorized =
+                error.status === HttpStatusCode.Unauthorized;
+
+              if (isUnauthorized) {
+                clearCookies();
+                router.push("/login");
+              }
+            }
+          },
+        }),
         mutationCache: new MutationCache({
           onError: (error) => {
             if (isAxiosError(error)) {
