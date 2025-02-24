9fefa0a6e8341b587b8ff675168db08dd0e9b7fe
Guilherme Deluca <gdelucapereira@gmail.com>
2025-02-23 22:28:11 -0300

    feat: updated login screen

--- src/screens/login/loginScreen.tsx
+++ src/screens/login/loginScreen.tsx
@@ -1,10 +1,11 @@
 import { Container } from "@/src/components/container";
 import { Input } from "@/src/components/form/input";
+import { InputPassword } from "@/src/components/form/inputPassword";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
+import { useRouter } from "expo-router";
 import { FormProvider, SubmitHandler, useForm } from "react-hook-form";
 import { StyleSheet } from "react-native";
-import { Button, Card, TextInput } from "react-native-paper";
+import { Button, Card, Text, useTheme } from "react-native-paper";
 import { z } from "zod";
 import { useLogin } from "./hooks/useLogin";
 
@@ -16,15 +17,20 @@ const LoginSchema = z.object({
 type LoginInputs = z.infer<typeof LoginSchema>;
 
 export const LoginScreen = () => {
+  const { colors } = useTheme();
   const form = useForm({
     resolver: zodResolver(LoginSchema),
   });
   const { handleSubmit } = form;
-  const [isSecureEntry, setIsSecureEntry] = useState(true);
   const { login, isLoading } = useLogin();
+  const router = useRouter();
 
   const onSubmit: SubmitHandler<LoginInputs> = (values) => {
     login(values);
+  };
+
+  const onPressSignup = () => {
+    router.replace("/(account)/signup");
   };
 
   return (
@@ -33,18 +39,8 @@ export const LoginScreen = () => {
         <Card.Title titleVariant="titleLarge" title="Login" />
         <Card.Content>
           <FormProvider {...form}>
-            <Input name="email" label={"Email"} />
-            <Input
-              label={"Password"}
-              name="password"
-              secureTextEntry={isSecureEntry}
-              right={
-                <TextInput.Icon
-                  onPress={() => setIsSecureEntry((prev) => !prev)}
-                  icon={isSecureEntry ? "eye" : "eye-off"}
-                />
-              }
-            />
+            <Input name="email" label={"Email"} keyboardType="email-address" />
+            <InputPassword label={"Password"} name="password" />
           </FormProvider>
         </Card.Content>
         <Card.Actions>
@@ -58,6 +54,12 @@ export const LoginScreen = () => {
             Submit
           </Button>
         </Card.Actions>
+        <Card.Content style={styles.contentSignup}>
+          <Text>Don't have an account?</Text>
+          <Text onPress={onPressSignup} style={{ color: colors.primary }}>
+            Signup now!
+          </Text>
+        </Card.Content>
       </Card>
     </Container>
   );
@@ -66,5 +68,9 @@ export const LoginScreen = () => {
 const styles = StyleSheet.create({
   submitButton: {
     flex: 1,
+  },
+  contentSignup: {
+    marginTop: 10,
+    alignItems: "center",
   },
 });
